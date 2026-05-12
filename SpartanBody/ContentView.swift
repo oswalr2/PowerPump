b6912ab60a374d870198181dfd3675fdd0b805cc
oswalr2 <oswal1318@gmail.com>
2026-05-13 00:29:06 +0100

    Initial Commit

---
+++ SpartanBody/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpartanBody
+//
+//  Created by oswaldo1318 on 13/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpartanBody/SpartanBodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpartanBodyApp.swift
+//  SpartanBody
+//
+//  Created by oswaldo1318 on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpartanBodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
